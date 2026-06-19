d44dbb9f38baaca6511544c7e7699878f7bfa020
Israt Jahan <eraisrat022@gmail.com>
2026-06-20 00:56:38 +0600

    Verify token and role based verify implement and vendor get/post

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT;
+const dns = require('node:dns');
+dns.setServers(['1.1.1.1', '1.0.0.1']); 
 
+const express = require('express');
+const cors = require("cors");
+const app = express()
+const port = process.env.PORT || 5000;
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const { jwtVerify, createRemoteJWKSet } = require('jose-cjs');
+
+app.use(cors());
+app.use(express.json());
+require('dotenv').config()
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
 const uri = process.env.MONGODB_URI;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -16,10 +31,88 @@ const client = new MongoClient(uri, {
   }
 });
 
+const JWKS = createRemoteJWKSet(new URL(`${process.env.CLIENT_URL}/api/auth/jwks`));
+
+const verifyToken = async(req,res,next) =>{
+  const authHeader = req.headers.authorization;
+  
+  if(!authHeader || !authHeader.startsWith('Bearer')){
+    return res.status(401).json({message : "Unauthorize"});
+  }
+
+  const token = authHeader.split(" ")[1];
+  
+  if(!token){
+    return res.status(401).json({message : "Unauthorize"});
+  }
+
+  try{
+    const {payload} = await jwtVerify(token, JWKS);
+    req.user = payload;
+    
+    next();
+  }catch(error){
+    return res.status(401).json({message : "Unauthorize"});
+  }
+}
+// Vendor verify
+const vendorVerify = async(req, res, next) => {
+  const user = req.user;
+  
+  if(user.role !== "vendor"){
+    return res.status(403).json({message : "Forbidden"});
+  }
+
+  next();
+};
+// User verify
+const userVerify = async(req,res,next) => {
+  const user = req.user;
+  if(user.role !== "user"){
+    return res.status(403).send({message : "Forbidden access"})
+  }
+
+  next();
+}
+// Admin verify
+const adminVerify = async(req,res,next) => {
+  const user = req.user;
+  if(user.role !== "admin"){
+    return res.status(403).send({message : "Forbidden access"})
+  }
+
+  next();
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const db = client.db("ticket_bari");
+    const userCollection = db.collection("user");
+    const ticketCollection = db.collection("tickets");
+    
+    // Vendor Ticket add
+    app.post('/vendor/tickets', verifyToken, vendorVerify, async(req,res) => {
+      const data = req.body;
+      const result = await ticketCollection.insertOne({
+        ...data,
+        userId: req.user.id,
+        userName: req.user.name,
+        userMail: req.user.email,
+        createdAt: new Date(),
+      });
+
+      res.json(result);
+    })
+    // Vendor Ticket get 
+    app.get('/vendor/tickets', async(req, res) => {
+      const result = await ticketCollection.find().toArray();
+
+      res.json(result);
+    })
+
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -29,11 +122,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
